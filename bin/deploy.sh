78d303d1b0eedf94f4b0396abc3697a21cb11801
SEKI Mayumi <seki-may@gnavi.co.jp>
2016-08-19 18:22:56 +0900

    #1

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -31,9 +31,9 @@ git checkout $DEPLOY_BRANCH_NAME
 git pull --rebase origin
 
 # WSから dist/ と .git/ 以外を削除
-# if test -d ${WORKSPACE} ; then
+if test -d ${WORKSPACE} ; then
 ls -A | grep -v "dist" | grep -v ".git" | xargs rm -rf | rm -rf .gitignore | rm -rf bin
-# fi
+fi
 
 # distディレクトリをroot展開
 cp -r dist/* .
